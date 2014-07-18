bb9d6330369de4fcb1b9bc292317d3cd230bd023
Hariharan Vadivelu <hariinfo@gmail.com>
2014-07-17 21:46:36 -0500

    added new template

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,5 @@
 /* google maps -----------------------------------------------------*/
 google.maps.event.addDomListener(window, 'load', initialize);
-
 function initialize() {
 
   /* position Amsterdam */
@@ -9,7 +8,7 @@ function initialize() {
   var mapOptions = {
     center: latlng,
     scrollWheel: false,
-    zoom: 8
+    zoom: 13
   };
 
   var marker = new google.maps.Marker({
@@ -18,26 +17,7 @@ function initialize() {
     animation: google.maps.Animation.DROP
   });
 
-  var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   marker.setMap(map);
 
 };
-
-
-
-   var im= new Array()//set your pictures here as an array
-	                im[0] = './images/1.JPG';
-	                im[1] = './images/2.JPG';
-	                im[2] = './images/3.JPG';
-	                im[3] = './images/4.JPG';
-	                im[4] = './images/5.JPG';
-	                im[5] = './images/6.JPG';
-	                var time = 2000; // set delay in miliseconds
-	                var q=1;
-	                function slideI(){
-	                document.getElementById('ima').setAttribute('src',im[q]);
-	                q++;
-	                if(q==im.length){q=0}
-	                setTimeout('slideI()',time);
-	                }
-	                onload=function(){setTimeout('slideI()',time)}
