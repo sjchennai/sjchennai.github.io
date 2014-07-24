5e8f1671d877d9d353b17b5ca90d34d61fcdc1ec
Hariharan Vadivelu <hariinfo@gmail.com>
2014-07-23 21:30:05 -0500

    Added News Section

--- javascripts/app.js
+++ javascripts/app.js
@@ -25,6 +25,19 @@ App.NewsView = Ember.View.extend({
 
 	  didInsertElement: function() {
 		  $('.ticker').ticker();
+
+		  $.fn.ticker.defaults = {
+		    random:        false, // Whether to display ticker items in a random order
+		    itemSpeed:     3000,  // The pause on each ticker item before being replaced
+		    cursorSpeed:   50,    // Speed at which the characters are typed
+		    pauseOnHover:  true,  // Whether to pause when the mouse hovers over the ticker
+		    finishOnHover: true,  // Whether or not to complete the ticker item instantly when moused over
+		    cursorOne:     '_',   // The symbol for the first part of the cursor
+		    cursorTwo:     '-',   // The symbol for the second part of the cursor
+		    fade:          true,  // Whether to fade between ticker items or not
+		    fadeInSpeed:   600,   // Speed of the fade-in animation
+		    fadeOutSpeed:  300    // Speed of the fade-out animation
+		};
 	}
 });
 
